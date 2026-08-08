a6a04eae2f5051ff390e833f4056706f2c493829
SuleimanByte <suleimanosman1023@gmail.com>
2026-08-08 16:10:40 +0300

    add expiration time both

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -2,8 +2,8 @@ import { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } from "../config/config.js";
 import jwt from "jsonwebtoken";
 
 export const generateAccessToken = (userId) => {
-    return jwt.sign({ id: userId }, ACCESS_TOKEN_SECRET)
+    return jwt.sign({ id: userId }, ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
 }
 export const generateRefreshToken = (userId) => {
-    return jwt.sign({ id: userId }, REFRESH_TOKEN_SECRET)
+    return jwt.sign({ id: userId }, REFRESH_TOKEN_SECRET, { expiresIn: "7d" });
 }
